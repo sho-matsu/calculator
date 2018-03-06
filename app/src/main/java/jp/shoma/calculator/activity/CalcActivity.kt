7502ddf8a5f5bbd366cff46a687947dbd9c0abc6
sho <born_in_april_89@yahoo.co.jp>
2018-03-06 18:40:46 +0900

    レイアウト調整 画面表示処理追加

--- app/src/main/java/jp/shoma/calculator/activity/CalcActivity.kt
+++ app/src/main/java/jp/shoma/calculator/activity/CalcActivity.kt
@@ -3,6 +3,8 @@ package jp.shoma.calculator.activity
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
 import jp.shoma.calculator.R
 import jp.shoma.calculator.databinding.ActivityCalcBinding
 import jp.shoma.calculator.model.CalcViewModel
@@ -14,6 +16,22 @@ class CalcActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calc)
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_calc)
-        mBinding.viewModel = CalcViewModel()
+        mBinding.viewModel = CalcViewModel(this)
+        val editText = mBinding.result
+        // キーボードを無効化
+        editText.keyListener = null
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                // 文字が入力されたあともフォーカスが一番最後にくるようにする
+                editText.setSelection(editText.length())
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+        })
     }
 }
--- app/src/main/java/jp/shoma/calculator/model/CalcViewModel.kt
+++ app/src/main/java/jp/shoma/calculator/model/CalcViewModel.kt
@@ -1,35 +1,148 @@
 package jp.shoma.calculator.model
 
+import android.content.Context
 import android.databinding.ObservableField
 import android.util.Log
 import android.view.View
+import android.widget.TextView
+import jp.shoma.calculator.R
 
 /**
  * Created by sho on 2018/03/05.
  */
-class CalcViewModel {
+class CalcViewModel(private val context: Context) {
+
+    companion object {
+        val TAG: String = this::class.java.name
+    }
+
     var preResult = ObservableField("0")
     var result = ObservableField("0")
+    private var operator: Operator = Operator.NONE
+    private var alreadyCalcValue = 0.0
+    private var nowInputValue = 0.0
 
-    fun onNumButtonPressed(view: View) {
-        // todo タップした数字を別な方法で伝搬したい
-        val num = view.tag as? Int
-        Log.d("CalcViewModel", "onNumButtonPressed : $num")
+    fun onButtonPressed(view: View) {
+        val textView = view as TextView
+        val pressedButton = textView.text.toString()
+        Log.d(TAG, "pressed : $pressedButton")
+
+        when (pressedButton) {
+            context.getString(R.string.button_0),
+            context.getString(R.string.button_1),
+            context.getString(R.string.button_2),
+            context.getString(R.string.button_3),
+            context.getString(R.string.button_4),
+            context.getString(R.string.button_5),
+            context.getString(R.string.button_6),
+            context.getString(R.string.button_7),
+            context.getString(R.string.button_8),
+            context.getString(R.string.button_9) -> {
+                setInputNum(pressedButton)
+                val num = pressedButton.toInt()
+            }
+            context.getString(R.string.button_plus),
+            context.getString(R.string.button_minus),
+            context.getString(R.string.button_multiply),
+            context.getString(R.string.button_divide) -> {
+                setInputOperator(pressedButton)
+            }
+            context.getString(R.string.button_equal) -> {
+                calculation()
+            }
+            context.getString(R.string.button_dot) -> {
+                setInputDot(pressedButton)
+            }
+            context.getString(R.string.button_delete) -> {
+                if (result.get().isNotEmpty()) {
+                    // 1文字だけ消す
+                    val str = if (result.get().length == 1) {
+                        "0"
+                    } else {
+                        result.get().substring(0, result.get().length - 1)
+                    }
+                    result.set(str)
+                }
+            }
+            context.getString(R.string.button_clear) -> {
+                result.set("0")
+                preResult.set("0")
+                operator = Operator.NONE
+                alreadyCalcValue = 0.0
+                nowInputValue = 0.0
+            }
+            else -> {
+                // NOP
+            }
+        }
     }
 
-    @Suppress("UNUSED_PARAMETER")
-    fun onOperatorButtonPressed(view: View) {
-        // todo タップした符号を別な方法で伝搬したい
-        val operator = view.tag as? String
-        Log.d("CalcViewModel", "onOperatorButtonPressed : $operator")
+    private fun calculation() {
+        when (operator) {
+            Operator.PLUS -> {
+
+            }
+            Operator.MINUS -> {
+
+            }
+            Operator.MULTIPLY -> {
+
+            }
+            Operator.DIVIDE -> {
+
+            }
+            else -> {
+                // NOP
+            }
+        }
     }
 
-    @Suppress("UNUSED_PARAMETER")
-    fun onDeleteButtonPressed(view: View) {
-        Log.d("CalcViewModel", "onDeleteButtonPressed")
+    /**
+     * 数字の入力を画面に反映
+     *
+     * @param input 入力文字
+     */
+    private fun setInputNum(input: String) {
+        if (result.get() == "0") {
+            result.set(input)
+        } else {
+            result.set(result.get() + input)
+        }
     }
 
-    fun onCommitButtonPressed() {
-        Log.d("CalcViewModel", "onCommitButtonPressed")
+    /**
+     * 算術演算子の入力を画面に反映
+     *
+     * @param input 入力文字
+     */
+    private fun setInputOperator(input: String) {
+        val last = result.get().substring(result.get().length -1)
+        if (input == last) return
+        operator = Operator.valueTypeOf(input)
+        // fixme 直前の入力が演算子だった場合、置き換える
+        result.set(result.get() + input)
+    }
+
+    /**
+     * 浮動小数点の入力を画面に反映
+     *
+     * @param input 入力文字
+     */
+    private fun setInputDot(input: String) {
+        if (result.get().contains(input)) return
+        result.set(result.get() + input)
+    }
+
+    enum class Operator(val symbol: String) {
+        PLUS("+"),
+        MINUS("-"),
+        MULTIPLY("*"),
+        DIVIDE("/"),
+        NONE("");
+
+        companion object {
+            fun valueTypeOf(symbol: String) =
+                values().firstOrNull { it.symbol == symbol } ?: NONE
+        }
     }
 }
