80db626a20efce968bd3aa771c14f9032d47855a
sho <born_in_april_89@yahoo.co.jp>
2018-03-14 18:29:36 +0900

    App Widgetの仮実装

---
+++ app/src/main/java/jp/shoma/calculator/CalcWidget.kt
@@ -0,0 +1,47 @@
+package jp.shoma.calculator
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class CalcWidget : AppWidgetProvider() {
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+//            updateAppWidget(context, appWidgetManager, appWidgetId)
+            val remoteViews = RemoteViews(context.packageName, R.layout.widget_calc)
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
+        }
+
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    companion object {
+
+        internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager,
+                                     appWidgetId: Int) {
+
+//            val widgetText = context.getString(R.string.appwidget_text)
+//            // Construct the RemoteViews object
+//            val views = RemoteViews(context.packageName, R.layout.widget_calc)
+//            views.setTextViewText(R.id.appwidget_text, widgetText)
+//
+//            // Instruct the widget manager to update the widget
+//            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
+
